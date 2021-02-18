bed83d72c4cfe6e18cfaa782c1d2e6a74e1ff3b0
hannared <hanna.korsak@gmail.com>
2021-02-18 15:30:43 +0100

    generated posts

---
+++ js/posts/render.js
@@ -0,0 +1,20 @@
+import generatePosts from './data.js';
+
+const images = generatePosts();
+
+const picturesContainer = document.querySelector('.pictures');
+
+const pictureTemplate = document.querySelector('#picture').content;
+
+const picturesFragment = document.createDocumentFragment();
+
+images.forEach(({ url, likes, comments }) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  picturesFragment.appendChild(pictureElement);
+});
+
+picturesContainer.appendChild(picturesFragment);
