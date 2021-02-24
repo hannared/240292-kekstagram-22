4a743f5c33175306b4987a3b4b247dec5ad6fa39
hannared <hanna.korsak@gmail.com>
2021-02-24 23:02:47 +0100

    added scalerfinished

--- js/posts/editor.js
+++ js/posts/editor.js
@@ -27,6 +27,10 @@ document.addEventListener('keydown', function (evt) {
 const buttonPlus = imageUploadModal.querySelector('.scale__control--bigger');
 const buttonMinus = imageUploadModal.querySelector('.scale__control--smaller');
 const scaleValue = imageUploadModal.querySelector('.scale__control--value');
+const imagePreview = imageUploadModal.querySelector('.img-upload__preview img');
+
+imagePreview.style = 'transform: scale(1.0)';
+scaleValue.value = '100%';
 
 buttonPlus.addEventListener('click', () => {
   scaleValue.value = parseInt(scaleValue.value) + 25;
@@ -34,14 +38,22 @@ buttonPlus.addEventListener('click', () => {
   if (scaleValue.value >= 100) {
     scaleValue.value = 100;
   }
+
+  const scale = scaleValue.value / 100;
+  imagePreview.style = `transform: scale(${scale})`;
+
   scaleValue.value += '%';
 });
 
 buttonMinus.addEventListener('click', () => {
   scaleValue.value = parseInt(scaleValue.value) - 25;
 
-  if (scaleValue.value <= 0) {
-    scaleValue.value = 0;
+  if (scaleValue.value <= 25) {
+    scaleValue.value = 25;
   }
+
+  const scale = scaleValue.value / 100;
+  imagePreview.style = `transform: scale(${scale})`;
+
   scaleValue.value += '%';
 });
