f3b2109f66dfb20cf1db5a6f2e69e5682ba2782f
Hanna Korsak <hannakorsak@Hannas-MacBook-Air.local>
2021-03-15 23:30:18 +0100

    added debounce

--- js/posts/filter.js
+++ js/posts/filter.js
@@ -6,8 +6,10 @@ const filterDefault = filter.querySelector('#filter-default');
 const filterRandom = filter.querySelector('#filter-random');
 const filterDiscussed = filter.querySelector('#filter-discussed');
 
+const RERENDER_DELAY = 500;
+
 filterDefault.addEventListener('click', () => {
-  render(images);
+  window._.debounce(() => render(images), RERENDER_DELAY);
 
   filterDefault.classList.add('img-filters__button--active');
   filterRandom.classList.remove('img-filters__button--active');
@@ -19,7 +21,7 @@ filterRandom.addEventListener('click', () => {
 
   // Get sub-array of first n elements after shuffled
   let selected = shuffled.slice(0, 10);
-  render(selected);
+  window._.debounce(() => render(selected), RERENDER_DELAY);
 
   filterRandom.classList.add('img-filters__button--active');
   filterDefault.classList.remove('img-filters__button--active');
@@ -27,6 +29,11 @@ filterRandom.addEventListener('click', () => {
 });
 
 filterDiscussed.addEventListener('click', () => {
+  const sorted = window._.sortBy(images, (image) => {
+    return image.comments.length;
+  });
+  window._.debounce(() => render(window._.reverse(sorted)), RERENDER_DELAY);
+
   filterDiscussed.classList.add('img-filters__button--active');
   filterRandom.classList.remove('img-filters__button--active');
   filterDefault.classList.remove('img-filters__button--active');
