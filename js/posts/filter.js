e7eba62a6a230e8b2f1ed8f1401ee9a850083055
Hanna Korsak <hannakorsak@Hannas-MacBook-Air.local>
2021-03-16 15:13:43 +0100

    fixed by comments

--- js/posts/filter.js
+++ js/posts/filter.js
@@ -7,34 +7,40 @@ const filterRandom = filter.querySelector('#filter-random');
 const filterDiscussed = filter.querySelector('#filter-discussed');
 
 const RERENDER_DELAY = 500;
+const ACTIVE_CLASS = 'img-filters__button--active';
+
+const debouncedRenderImages = window._.debounce((images) => {
+  render(images);
+}, RERENDER_DELAY);
 
 filterDefault.addEventListener('click', () => {
-  window._.debounce(() => render(images), RERENDER_DELAY);
+  debouncedRenderImages(images);
 
-  filterDefault.classList.add('img-filters__button--active');
-  filterRandom.classList.remove('img-filters__button--active');
-  filterDiscussed.classList.remove('img-filters__button--active');
+  filterDefault.classList.add(ACTIVE_CLASS);
+  filterRandom.classList.remove(ACTIVE_CLASS);
+  filterDiscussed.classList.remove(ACTIVE_CLASS);
 });
 
-filterRandom.addEventnListener('click', () => {
+filterRandom.addEventListener('click', () => {
   const shuffled = images.sort(() => 0.5 - Math.random());
 
-  // Get sub-array of first n elements after shuffled
   let selected = shuffled.slice(0, 10);
-  window._.debounce(() => render(selected), RERENDER_DELAY);
 
-  filterRandom.classList.add('img-filters__button--active');
-  filterDefault.classList.remove('img-filters__button--active');
-  filterDiscussed.classList.remove('img-filters__button--active');
+  debouncedRenderImages(selected);
+
+  filterRandom.classList.add(ACTIVE_CLASS);
+  filterDefault.classList.remove(ACTIVE_CLASS);
+  filterDiscussed.classList.remove(ACTIVE_CLASS);
 });
 
 filterDiscussed.addEventListener('click', () => {
   const sorted = window._.sortBy(images, (image) => {
     return image.comments.length;
   });
-  window._.debounce(() => render(window._.reverse(sorted)), RERENDER_DELAY);
 
-  filterDiscussed.classList.add('img-filters__button--active');
-  filterRandom.classList.remove('img-filters__button--active');
-  filterDefault.classList.remove('img-filters__button--active');
+  debouncedRenderImages(window._.reverse(sorted));
+
+  filterDiscussed.classList.add(ACTIVE_CLASS);
+  filterRandom.classList.remove(ACTIVE_CLASS);
+  filterDefault.classList.remove(ACTIVE_CLASS);
 });
